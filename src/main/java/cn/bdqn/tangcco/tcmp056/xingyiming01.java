5343faa05943b8a7d4f02289c23e3cc0cfaac788
‘xingyiming’ <260892184@qq.com>
2017-08-03 18:44:22 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/xingyiming01.java
@@ -0,0 +1,14 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/3.
+ */
+public class xingyiming01 {
+
+    @Test
+    public void xingyiming01() {
+        System.out.println("hello word");
+    }
+}
